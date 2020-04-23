eaa730af44c305257376cbac99585dd5f65772b7
Francisco Bohorquez <boho111805@siigo.com>
2020-04-23 07:26:51 -0500

    Register services on IoC Layer. Configure dependencyContainer on presentation Layer

---
+++ CleanArch.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Infrastructure.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- CleanArch.Mvc/Startup.cs
+++ CleanArch.Mvc/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArch.Infrastructure.Data;
+using CleanArch.Infrastructure.IoC;
 
 namespace CleanArch.Mvc
 {
@@ -38,6 +39,7 @@ namespace CleanArch.Mvc
             services.AddDbContext<UniversityDBContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UniversityDBConnection")));
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,6 +71,11 @@ namespace CleanArch.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
